d8c74a1c62fb4944856182cc99b40692a6a5c830
Kelvin-Ziqi-Zhao <Kelvin.Ziqi.Zhao@outlook.com>
2023-10-25 10:12:37 +0800

    Create initialise function
    append include

--- binary_tree.py
+++ binary_tree.py
@@ -7,3 +7,24 @@ class TreeNode:
             self.Data = Data
             self.RgihtPointer = RightPointer    
 
+#Check Functions
+
+#Initialise Function
+def Initialise(Length):
+    #Define Variables
+    global Null
+    Null = -1
+    global RootPointer
+    RootPointer = Null
+    global FreePointer
+    FreePointer = 0
+    global Tree
+    Tree = []
+    #Fill in the list
+    for i in range(0,Length-1):
+        Tree.append(TreeNode(i+1,"",Null))
+    if Length > 0:
+        Tree.append(TreeNode(Null,"",Null))
+#Method Function
+
+#Main Thread
