c3a35b13b177574fb2db6c01d43bfbdfaadb10c9
Kelvin-Ziqi-Zhao <Kelvin.Ziqi.Zhao@outlook.com>
2023-10-25 10:03:50 +0800

    Data structure create

---
+++ binary_tree.py
@@ -0,0 +1,9 @@
+#Author: Kelvin Zhao 
+
+#Data Structure
+class TreeNode:
+    def __init__ (self,LeftPointer,Data,RightPointer):
+            self.LeftPointer = LeftPointer
+            self.Data = Data
+            self.RgihtPointer = RightPointer    
+
