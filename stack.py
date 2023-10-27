bf954b5f82836db7c1db1287b6acd32d7b565808
Kelvin-Ziqi-Zhao <Kelvin.Ziqi.Zhao@outlook.com>
2023-10-27 15:12:39 +0800

    Add Pop function

--- stack.py
+++ stack.py
@@ -1,21 +1,40 @@
-Stack = []
-StackTopPointer = 0
+
 
 def InitialiseStack(Size):
+    global Stack,StackMaxSize,StackTopPointer,Null
+    Stack = []
+    Null = -1
+    StackTopPointer = Null
+    StackMaxSize = Size
     for i in range(Size):
-        Stack.append(0)
-        global StackMaxSize
-        StackMaxSize = Size
+        Stack.append(Null)
+
 def Push(NewItem):
     global StackMaxSize, StackTopPointer
     if StackTopPointer < StackMaxSize:
-        Stack[StackTopPointer] = NewItem
         StackTopPointer += 1
+        Stack[StackTopPointer] = NewItem
         return True
     else:
         return False
+    
+def Pop():
+    global StackTopPointer
+    Item = Stack[StackTopPointer]
+    if StackTopPointer > Null:
+        Stack[StackTopPointer] = Null
+        StackTopPointer -= 1
+        return(Item)
+        
+    else:
+        return(False)
+
 
 InitialiseStack(10)
 Push("A")
 Push("B")
+print(Stack)
+print(Pop(),StackTopPointer)
+print(Stack)
+print(Pop(),StackTopPointer)
 print(Stack)
