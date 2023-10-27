23f145586070f975e620d595dc4916c9bc52a9c9
Kelvin-Ziqi-Zhao <Kelvin.Ziqi.Zhao@outlook.com>
2023-10-27 14:58:53 +0800

    Add InitialiseStack function

--- stack.py
+++ stack.py
@@ -1,10 +1,21 @@
-Stack = [NewItem]
-StackTopPointer = len(Stack)
+Stack = []
+StackTopPointer = 0
 
+def InitialiseStack(Size):
+    for i in range(Size):
+        Stack.append(0)
+        global StackMaxSize
+        StackMaxSize = Size
 def Push(NewItem):
-    if StackTopPointer < len(Stack):
-        StackTopPointer += StackTopPointer
-        StackTopPointer = NewItem
+    global StackMaxSize, StackTopPointer
+    if StackTopPointer < StackMaxSize:
+        Stack[StackTopPointer] = NewItem
+        StackTopPointer += 1
         return True
     else:
         return False
+
+InitialiseStack(10)
+Push("A")
+Push("B")
+print(Stack)
