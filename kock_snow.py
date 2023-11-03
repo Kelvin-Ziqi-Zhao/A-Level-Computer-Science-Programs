fa0c1cad3244f40f695d7ac70eef65ca41e06a5b
Kelvin-Ziqi-Zhao <Kelvin.Ziqi.Zhao@outlook.com>
2023-11-03 14:48:16 +0800

    Basic draw algorithm

--- kock_snow.py
+++ kock_snow.py
@@ -1,3 +1,17 @@
 from turtle import *
 
+def snow(Level,Length):
+    
+    forward(Length/3)
+    left(60)
+    forward(Length/3)
+    right(120)
+    forward(Length/3)
+    left(60)
+    forward(Length/3)
+    home()
+
+
+snow(1,100)
 mainloop()
+
