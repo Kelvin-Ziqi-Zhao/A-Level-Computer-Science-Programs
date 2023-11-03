a0bb4b7456abb9107328fcecbb85f4b1b17b3754
Kelvin-Ziqi-Zhao <Kelvin.Ziqi.Zhao@outlook.com>
2023-11-03 15:13:36 +0800

    First tiem to draw a snowflake

--- kock_snow.py
+++ kock_snow.py
@@ -1,17 +1,26 @@
 from turtle import *
+n=3
 
 def snow(Level,Length):
+
+    if Level > 1:
+        snow(Level-1,Length)
+        left(60)
+        snow(Level-1,Length)
+        right(120)
+        snow(Level-1,Length)
+        left(60)
+        snow(Level-1,Length)
+    else:
+        forward(Length/3)
     
-    forward(Length/3)
-    left(60)
-    forward(Length/3)
+
+speed(0)
+penup()
+goto(-100,50)
+pendown()
+for i in range(3):
+    snow(n,200/n)
     right(120)
-    forward(Length/3)
-    left(60)
-    forward(Length/3)
-    home()
-
-
-snow(1,100)
 mainloop()
 
