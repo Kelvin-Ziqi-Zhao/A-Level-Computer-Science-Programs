15961c1f8308bbf6f10ef8602372ffb4ceb85be2
Kelvin-Ziqi-Zhao <Kelvin.Ziqi.Zhao@outlook.com>
2023-12-06 09:50:35 +0800

    Complete todo

--- bank_account.py
+++ bank_account.py
@@ -1,6 +1,17 @@
+class BankAccount:
+    def __init__(self):
+        self.balance = 10
+    def __init__(self, num):
+        self.balance = num + 10
+    def getBalance(self):
+        return self.__balance
+    def deposit(self, num):
+        self.balance += sum
+    def withdraw(self, num):
+        self.balance -= sum
+        
 #TODO: Implement a BankAccount that pays a bonus of $10 for each new account.
 #A bank account has a balance that can be changed by deposits and withdrawals.
-...
 
 account1 = BankAccount()
 print("Balance:", account1.getBalance())
@@ -9,6 +20,8 @@ account1.deposit(200)
 print("Balance after deposit 200:", account1.getBalance())
           
 #your work here: withdraw 50 and output the balance of account1
+account1.withdraw(50)
+print(account1.getBalance)
       
 account2 = BankAccount(1000)
 print("Balance:", account2.getBalance())
@@ -17,3 +30,5 @@ account2.withdraw(200)
 print("Balance after withdraw 200: ", account2.getBalance())
 
 #your work here: deposit 50 and output the balance of account2
+account2.deposit(50)
+print(account2.getBalance)
