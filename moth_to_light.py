43f52dc121ed1a071efa421844bb6aa852bcd569
Kelvin-Ziqi-Zhao <Kelvin.Ziqi.Zhao@outlook.com>
2023-12-06 10:31:46 +0800

    Algorithm complelte

--- moth_to_light.py
+++ moth_to_light.py
@@ -1,9 +1,13 @@
 class Moth():
     def __init__(self, initPosition):
         self.__position = initPosition
+    
+    def moveToLight(self,lightPosition):
+        self.__position = (self.__position + lightPosition)/2
 
     def getPosition(self):
         return self.__position
     
 gypsy = Moth(10)
+gypsy.moveToLight(20)
 print(gypsy.getPosition())
