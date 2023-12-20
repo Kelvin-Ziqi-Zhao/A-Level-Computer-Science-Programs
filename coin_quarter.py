4da9e002893c83fbe90acb9bbfe30d18d11ade54
Kelvin-Ziqi-Zhao <Kelvin.Ziqi.Zhao@outlook.com>
2023-12-20 10:03:29 +0800

    Algorithm complete

--- coin_quarter.py
+++ coin_quarter.py
@@ -4,24 +4,24 @@ The getDescription method should yield a string such as
 
 #A coin with a monetary value.
 class Coin:
-    def __init__(self, name, monetary): #Constructs a coin.
+    def __init__(self,monetary, name): #Constructs a coin.
         self.__name = name #@param aName the name of the coin
         self.__monetary = monetary #@param aValue the monetary value of the coin.
 
     def getCoinValue(self): #Gets the coin value.
-       return __monetary #@return the value
+       return self.__monetary #@return the value
     
     def getDescription(self): #Gets the coin description.
-       return __name + "," + "value=" + __monetary #return a description of this coin
+       return self.__name + ", " + "value=" + str(self.__monetary) #return a description of this coin
     
 #A quarter with a state theme.
 class Quarter(Coin):
-    def __init__(self, name, monetary, state):#Constructs a quarter.
-        super().__init__(monetary, name)
-        __state = state
+    def __init__(self, state):#Constructs a quarter.
+        super().__init__(0.25, "Quater")
+        self.__state = state
 
     def getDescription(self):
-        return __name + "," + "value=" + __monetary + "state=" + __state
+        return Coin.getDescription(self) + ", state=" + self.__state
 
 c = Quarter("Michigan")
 print(c.getDescription())
